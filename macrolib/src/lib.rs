3da78ae0ca1271fdfea3aab604839134061c552b
Anton Afanasyev <anton@doubleasoftware.com>
2025-08-10 16:16:21 -0700

    cleanup

--- macrolib/src/lib.rs
+++ macrolib/src/lib.rs
@@ -8,14 +8,13 @@ pub fn attrib_macro(_args: TokenStream, input: TokenStream) -> TokenStream {
     let parsed_item = parse_macro_input!(input as Item);
 
     // Debug output to see what attributes are available
-    let attrs: &Vec<syn::Attribute> = match &parsed_item {
+    let attrs= match &parsed_item {
         Item::Fn(item_fn) => &item_fn.attrs,
         Item::Struct(item_struct) => &item_struct.attrs,
         Item::Enum(item_enum) => &item_enum.attrs,
         _ => &vec![],
     };
 
-    // Print debug information about attributes
     eprintln!("=== PROC MACRO DEBUG ===");
     eprintln!("Number of attributes found: {}", attrs.len());
     for (i, attr) in attrs.iter().enumerate() {
@@ -23,20 +22,9 @@ pub fn attrib_macro(_args: TokenStream, input: TokenStream) -> TokenStream {
     }
     eprintln!("=== END DEBUG ===");
 
-    // Generate the same output as before but preserve information
     quote! {
-      #[macrolib::attrib_macro2]
       fn foo() -> &'static str {
         "foo"
-      }
-    }
-    .into()
-}
-#[proc_macro_attribute]
-pub fn attrib_macro2(_args: TokenStream, _input: TokenStream) -> TokenStream {
-    quote! {
-      fn foo2() -> &'static str {
-        "foo2"
       }
     }
     .into()
