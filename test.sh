763961148e7cdf072b73547529856c80ace5d211
Anton Afanasyev <anton@doubleasoftware.com>
2025-08-15 14:30:48 -0700

    cleanup test script, print out output

--- test.sh
+++ test.sh
@@ -1,16 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 TCNAME="${1}"
 
+_D=$(dirname $(realpath ${0} ) )
+
+# Create temp files
+out=$(mktemp)
+err=$(mktemp)
+
+# Clean up on exit
+trap "rm -f '$out' '$err'" EXIT
+
+cd "${_D}"
 cargo clean
-OUTPUT=$(cargo ${TCNAME:+"+${TCNAME}"} clippy 2>&1)
-_RET=$?
-if [[ $_RET -ne 0 ]]; then
-	echo $OUTPUT >&2
+if ! cargo ${TCNAME:+"+${TCNAME}"} clippy >"$out" 2>"$err"; then
+	_RET=$?
+	cat "$out"
+	cat "$err" >&2
 	exit $_RET
 fi
 
-if echo "$OUTPUT" | grep -q "use of a disallowed macro \`macrolib::attrib_macro\`"; then
-	exit 1
+cat "$out"
+cat "$err" >&2
+if grep -q "use of a disallowed macro \`macrolib::attrib_macro\`" "$out" "$err"; then
+	exit 125
 else
 	exit 0
 fi
